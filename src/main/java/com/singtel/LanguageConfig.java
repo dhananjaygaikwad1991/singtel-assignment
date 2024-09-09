a315b8a222e68847132e7beca03dd9ca691d86eb
Dhananjay Gaikwad <dgaikwad@oldmutual.com>
2024-09-09 21:24:04 +0530

    test commit 1

--- src/main/java/com/singtel/LanguageConfig.java
+++ src/main/java/com/singtel/LanguageConfig.java
@@ -15,7 +15,7 @@ public class LanguageConfig {
     @Bean
     Map<String, String> languages() {
         logger.info("********* SETUP LANGUAGES *************");
-
+        logger.info("********* SETUP LANGUAGES1 *************");
         Map<String, String> languages = new HashMap<String, String>();
         languages.put("French", "cocorico");
         languages.put("German", "kikeriki");
